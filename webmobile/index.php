a0cc69c3f6ac441330c9e38d0b9f124e4f1c5803
Ante696 <58498633+Ante696@users.noreply.github.com>
2021-06-03 22:11:27 +0700

    Add files via upload

---
+++ webmobile/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Web Responsive</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		<h1 align="center">Indah Sepanjang Mata Memandang</h1>
+	</div> <!--/ .header -->
+	<div class="main">
+		<div class="left">
+			<h3><p>Website ini berisi Informasi Tempat Wisata yang ada di Yogyakarta guna meningkatkan minat masyarakat terhadap pariwisata lokal dan menghidupkan perekonimoan lokal dimasa pandemi</p></h3>
+			
+		</div> <!--/ .header -->
+		<div class="middle">
+			<h3 style="text-align: center;">Artikel Terbaru</h3>
+			<p><img src="https://cdn.idntimes.com/content-images/post/20190528/000009-02-wisata-alam-di-jogja-hutan-mangrove-800x450min-ccpdm-85caadd989d9452fe63352e0bf2477f6_600x400.jpg" alt=""></p>
+			<strong>Wisata Hutan Mangrove Pasir Mendit</strong>
+			<p>Hutan Mangrove merupakan hutan yang berada di lingkungan perairan payau dan sangat dipengaruhi oleh pasang surut air laut. Hutan Mangrove Pasir Mendit merupakan salah satu destinasi wisata yang berada di Pantai Congot, Kapanewon Temon, Kulon progo yang dilengkapi dengan beberapa spot foto dan jogging track </p>
+
+			<p><img src="https://cdn.idntimes.com/content-images/community/2017/09/1-geblek-985d17926430c21cb62042794c7bdb16_600x400.jpg" alt=""></p>
+			<strong>Geblek Sengek</strong>
+			<p>Geblek merupakan salah satu makanan tradisional khas dari Kabupaten Kulon Progo ang terbuat dari tepung tapioka yang dimakan dengan Sengek Tempe. Sengek Tempe terbuat dari tempe benguk atau kacang koro ang dimasak dengan santan gurih nan kental. </p>
+		</div> <!--/ .middle -->
+		<div class="right">
+			<a href="page2.php" class='button'>Destinasi</a><br/><br/>
+			<a href="page3.html" class='button'>Tambah API</a><br/><br/>
+		</div> <!--/ .right -->
+	</div> <!--/ .main -->
+	<div class="footer">
+		<p align="center">Ante Wahyu_185411135</p>
+	</div> <!--/ .footer -->
+</div>
+</body>
+</html>
---
+++ webmobile/page2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Destinasi Wisata</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="list">
+	<div class="atas">
+		<h1 align="center">Daftar Destinasi Wisata Yogyakarta</h1>
+	</div> <!--/ .header -->
+
+	<div class="main">
+		<div class="left2">
+			<h3><p>Halaman ini berisi Daftar Destinasi Wisata di Yogyakarta</p></h3>
+			
+		</div> <!--/ .header -->
+		<div class="middle">
+			
+			<p><img src="https://travelspromo.com/wp-content/uploads/2019/04/Area-Studio-Alam-Gamplong-Fis-Ihsani-640x360.jpg" alt=""></p>
+			<strong><a href="https://www.instagram.com/gamplong_studio/" class="user-image img-responsive">Studio Alam Gamplong</a></strong><br><br>
+
+			<p><img src="https://cdn.idntimes.com/content-images/post/20190528/000009-02-wisata-alam-di-jogja-hutan-mangrove-800x450min-ccpdm-85caadd989d9452fe63352e0bf2477f6_600x400.jpg" alt=""></p>
+			<strong><a href="https://www.google.com/maps/place/Wisata+Hutan+Mangrove+Wana+Tirta/@-7.8933246,110.0110709,15z/data=!3m1!4b1!4m5!3m4!1s0x2e7ae6ca8b063e9b:0xce10d1cf69a748e8!8m2!3d-7.8933459!4d110.0198257" class="user-image img-responsive">Hutan Mangrove Pasir Mendit</a></strong><br><br>
+			
+			<p><img src="https://explorewisata.com/wp-content/uploads/2019/10/heha-sky-view-jogja.jpg" class="user-image img-responsive" alt=""></p>
+			<strong><a href="https://hehaskyview.com/">Heha Sky View</a></strong><br><br>
+
+			<p><img src="https://indonesia.tripcanvas.co/id/wp-content/uploads/sites/2/2019/12/6-1-Bukit-Panguk-Kediwung-by-bessieswastika-@-feedut_-@-fathurohman7-@-rafirss-740x740.jpg" class="user-image img-responsive" alt=""></p>
+			<strong><a href="https://www.google.com/maps/place/Bukit+Panguk+Kediwung/@-7.9583695,110.4385735,17z/data=!3m1!4b1!4m5!3m4!1s0x2e7bacb24995d023:0x95aec26c5e9774a2!8m2!3d-7.9583695!4d110.4407622?hl=id">Bukit Panguk Kediwung</strong><br><br>			
+			
+			<p><img src="https://indonesia.tripcanvas.co/id/wp-content/uploads/sites/2/2018/02/8-c-2-Curug-Pulosari-by-mardheni-740x515.jpg" class="user-image img-responsive" alt=""></p>
+			<strong><a href="https://indonesia.tripcanvas.co/id/wp-content/uploads/sites/2/2018/02/8-c-2-Curug-Pulosari-by-mardheni-740x515.jpg">Curug Pulo Sari Pajangan Bantul</strong><br><br>
+		</div> <!--/ .middle -->
+		<div class="right">
+			<a href="index.php" class='button'>Home</a><br/><br/>
+			<a href="page3.html" class='button'>Tambah API</a><br/><br/>
+		</div> <!--/ .right -->
+	</div> <!--/ .main -->
+	<div class="footer">
+		<p align="center">Ante Wahyu_185411135</p>
+	</div> <!--/ .footer -->
+</div>
+</body>
+</html>
